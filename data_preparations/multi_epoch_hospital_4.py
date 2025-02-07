888dda612362de084b395a64681d74c8491e564f
wcjjzz <1401581164@qq.com>
2025-02-07 16:39:19 +0800

    修改【医院数据集】【multi预处理】代码

--- data_preparations/multi_epoch_hospital_4.py
+++ data_preparations/multi_epoch_hospital_4.py
@@ -5,12 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import mne
-
+from sklearn.model_selection import KFold
 # from mne.time_frequency import tfr_morlet, psd_multitaper, psd_welch
 import h5py
 
 #path必须以\\结尾，否则传入的路径与文件名拼接错误
-hospital_path = r"C:\Users\Tian_Yumi\Downloads\BaiduNetdiskDownload\sleep_data_PSG(depression)\2019.12.10-2019.12.11\2017-2019\汉字\depression\\"
+hospital_path = "C:\\Users\\LYT\\Desktop\\数据集\\hospital_depression\\depression_pinyin - 副本\\"
 
 def signal_extract_sequential_hospital(edf_anno_list, channel='eeg1', filter=True, freq=[0.2, 40], stride=3):
 
@@ -22,11 +22,11 @@ def signal_extract_sequential_hospital(edf_anno_list, channel='eeg1', filter=Tru
         'F3', 'F4', 'C3', 'C4', 'O1', 'O2',
         'M1', 'M2',
         'E1', 'E2',
-        'ECG2', 'ECG1',
-        'Chin1', 'Chin2', 'LEG/L', 'LEG/R',
-        'AIRFLOW', 'ABDO', 'THOR', 'Snore', 'Pos Sensor', 'Ox Status', 'Pulse', 'SpO2', 'Nasal Pressure', 'CPAP Flow',
-        'CPAP Press', 'Pleth', 'Sum', 'PTT', 'Derived HR', 'Respiratory rate', 'Light', 'Manual Pos'
-        )
+        'ECG1', 'ECG2',
+        'Chin1', 'Chin2', 'Chin3', 'LEG/L', 'LEG/R',
+        'Airflow', 'Abdo', 'Thor', 'Snore', 'Sum', 'PosSensor', 'Ox Status', 'Pulse', 'SpO2', 'Nasal Pressure', 'CPAP Flow',
+        'CPAP Press', 'Pleth', 'Sum', 'Derived HR', 'Light', 'Manual Pos', 'Respiratory Rate'
+    )
 
     first_sub_flag = 0
 
@@ -37,7 +37,7 @@ def signal_extract_sequential_hospital(edf_anno_list, channel='eeg1', filter=Tru
 
         # 【改数据获取】
             signal2idx = {"eeg1": 0, "eeg2": 1, "eeg3": 2, "eeg4": 3, "eeg5": 4, "eeg6": 5,
-                          "eog1": 8, "eog2": 9}
+                                "eog1": 8, "eog2": 9}
             all_channels_list = list(all_channels)
             all_channels_list.remove(all_channels[signal2idx[channel]])
             exclude_channels = tuple(all_channels_list)
@@ -52,11 +52,10 @@ def signal_extract_sequential_hospital(edf_anno_list, channel='eeg1', filter=Tru
         #         "Sleep stage W": 0, "Sleep stage 1": 1, "Sleep stage 2": 2, "Sleep stage 3": 3,
         #          "Sleep stage R": 4, "Sleep stage ?": 5, "Movement time": 6}
             ann2label = {
-                "Sleep stage W": 0, "Sleep stage 1": 1, "Sleep stage 2": 2, "Sleep stage 3": 3, "Sleep stage 4": 4, "Sleep stage R": 5}
+                "Sleep stage W": 0, "Sleep stage 1": 1, "Sleep stage 2": 2, "Sleep stage 3": 3, "Sleep stage R": 4}
 
             ann2label_without_unknown_stages = {
-                "Sleep stage W": 0, "Sleep stage 1": 1, "Sleep stage 2": 2, "Sleep stage 3": 3, "Sleep stage 4": 4,
-                "Sleep stage R": 5}
+                "Sleep stage W": 0, "Sleep stage 1": 1, "Sleep stage 2": 2, "Sleep stage 3": 3, "Sleep stage R": 4}
 
             # annot.crop(annot[1]['onset'] - 30 * 60, annot[-2]['onset'] + 30 * 60)
 
@@ -120,10 +119,8 @@ def signal_extract_sequential_hospital(edf_anno_list, channel='eeg1', filter=Tru
                                 temp_labels.append(2)
                             if sleep_stage == {"Sleep stage 3": 3}:
                                 temp_labels.append(3)
-                            if sleep_stage == {"Sleep stage 4": 4}:
-                                temp_labels.append(3)
-                            if sleep_stage == {"Sleep stage R": 5}:
-                                temp_labels.append(5)
+                            if sleep_stage == {"Sleep stage R": 4}:
+                                temp_labels.append(4)
 
                     sig_epochs.append(temp_epochs)
                     label_epochs.append(temp_labels)
@@ -152,23 +149,20 @@ def signal_extract_sequential_hospital(edf_anno_list, channel='eeg1', filter=Tru
     return main_ext_raw_data, main_labels, main_sub_len, main_mean, main_std
 
 
-
 if __name__ == '__main__':
-
     # Separate Subjects into 5 groups
-    from sklearn.model_selection import KFold
+    # 【改】======================================================================
 
-#【改】======================================================================
     edf_anno_list = []
     # 查找所有的edf文件和对应的注释文件【列表edf_anno_list】
     for filename in os.listdir(hospital_path):
-        if fnmatch.fnmatch(filename, '*_RK_mne_Annotation.txt'):  # 查找以 "_RK_mne_Annotation.txt" 结尾的文件
-            tmp = filename.split('_RK_mne_Annotation.txt')[0]  # 提取文件名前缀部分，比如 chenfang20170510
+        if fnmatch.fnmatch(filename, '*_mne_Annotation.txt'):  # 查找以 "_mne_Annotation.txt" 结尾的文件
+            tmp = filename.split('_mne_Annotation.txt')[0]  # 提取文件名前缀部分，比如 chenfang20170510
             for i in os.listdir(hospital_path):
                 if fnmatch.fnmatch(i, tmp + '.edf'):  # 查找以相同前缀并以 .edf 结尾的文件，比如chenfang20170510.edf
                     edf_anno_list.append((i, filename))  # 将edf文件和注释文件的元组添加到列表中
                     print(i + " matched " + filename)
-
+    print(f"Found {len(edf_anno_list)} EDF and annotation file pairs.")
 
     # 5折交叉验证
     fivefold_list = []
@@ -183,39 +177,42 @@ if __name__ == '__main__':
     for train_index, test_index in kf.split(edf_anno_list):
         fivefold_list.append((train_index, test_index))
 
-    # 遍历5个fold
-    for i, (train_index, test_index) in enumerate(kf.split(edf_anno_list)):
-        # 获取当前fold的信号文件和注释文件列表
-        train_files = [edf_anno_list[i] for i in train_index]  # 信号文件和注释文件的元组列表
+# 遍历每个fold
+for i, (train_index, test_index) in enumerate(fivefold_list):
+    # 获取当前fold的训练集信号文件和注释文件列表
+    train_files = [edf_anno_list[i] for i in train_index]  # 信号文件和注释文件的元组列表
 
+    # 打印当前组的所有 EDF 文件名
+    print(f"Fold {i + 1} - Train Files EDF:")
+    for file_pair in train_files:
+        print(f"EDF File: {file_pair[0]}")
 
-        # 调用 signal_extract_sequential_hospital 函数
-        main_ext_raw_data, main_labels, main_sub_len, main_mean, main_std = signal_extract_sequential_hospital(
-            train_files, channel='eeg1', filter=True, freq=[0.2, 40], stride=3)
+    # 调用 signal_extract_sequential_hospital 函数
+    main_ext_raw_data, main_labels, main_sub_len, main_mean, main_std = (
+        signal_extract_sequential_hospital(train_files, channel='eeg1', filter=True, freq=[0.2, 40], stride=3))
 
-        print(f"Train data shape : {main_ext_raw_data.shape}, Train label shape : {main_labels.shape}")
+    print(f"Train data shape : {main_ext_raw_data.shape}, Train label shape : {main_labels.shape}")
 
-        # 保存结果
-        eeg1 = main_ext_raw_data
-        labels = main_labels
-        eeg1_m = main_mean
-        eeg1_std = main_std
+    # 保存结果
+    eeg1 = main_ext_raw_data
+    labels = main_labels
+    eeg1_m = main_mean
+    eeg1_std = main_std
 
-        # 将结果保存为 h5 文件
-        with h5py.File(f'{save_path}/x{i}.h5', 'w') as f:
-            f.create_dataset('eeg1', data=eeg1)
+    # 保存为h5文件
+    with h5py.File(f'{save_path}/x{i}.h5', 'w') as f:
+        f.create_dataset('eeg1', data=eeg1)
 
-        with h5py.File(f'{save_path}/y{i}.h5', 'w') as f:
-            f.create_dataset('labels', data=labels)
+    with h5py.File(f'{save_path}/y{i}.h5', 'w') as f:
+        f.create_dataset('labels', data=labels)
 
-        with h5py.File(f'{save_path}/mean{i}.h5', 'w') as f:
-            f.create_dataset('mean', data=eeg1_m)
+    with h5py.File(f'{save_path}/mean{i}.h5', 'w') as f:
+        f.create_dataset('mean', data=eeg1_m)
 
-        with h5py.File(f'{save_path}/std{i}.h5', 'w') as f:
-            f.create_dataset('std', data=eeg1_std)
+    with h5py.File(f'{save_path}/std{i}.h5', 'w') as f:
+        f.create_dataset('std', data=eeg1_std)
 
-        # 打印每个fold的结果信息
-        print(f"Fold {i + 1} complete. Results saved to the corresponding h5 files.")
+    print(f"Fold {i + 1} complete. Results saved to the corresponding h5 files.")
 
 
 
@@ -234,4 +231,3 @@ if __name__ == '__main__':
 # hf = h5py.File(f'{save_path}/std1.h5', 'w')
 # hf.create_dataset('data', data=eeg1_std1)
 # hf.close()
-
